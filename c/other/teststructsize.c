df69da579cf64b1087e9a847d3f575a1ee1db23c
song-syj <yangjiesun2012@gmail.com>
2014-02-17 16:20:17 +0800

    distinguish struct sizes in linux and windows

--- c/other/teststructsize.c
+++ c/other/teststructsize.c
@@ -6,8 +6,29 @@ struct test_struct {
 	char z;
 	double w;
 };
+
+/* 24 bytes in Windows  */
+struct S1 {
+	char i;
+	char j;
+	int t;
+	double d;
+	char c;
+};
+
+/* 16 bytes in windows*/
+#pragma pack(2)
+struct S2 {
+	char i;
+	char j;
+	int t;
+	double d;
+	char c;
+};
+#pragma pack()
+
 int main(void)
 {
-	printf("%d",sizeof(struct test_struct));
+	printf("%d",sizeof(struct S1));
 	return 0;
 }
