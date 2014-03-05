0b0304aa5398277dafc2a1e710f7b3a90a3d3987
song-syj <yangjiesun2012@gmail.com>
2014-03-06 00:29:24 +0800

    add solution for chapter 5

---
+++ csapp/chapter5.c
@@ -0,0 +1,41 @@
+/* My solution to homework problems of chapter 5 in caspp
+ * Date: 2014.03.06
+ * Reference: Stackverflow website and others
+ */
+
+/* 5.15 */
+/* A. There is a dependency between successive values of program value sum stored in register %xmm1. The loop computes a new value for sum by adding the old value with the product of udata[i] and vdata[i] stored in register %xmm0; Another dependency between succesive values of program vaule i. */
+
+/* B. The latency of the floating-point adder limits the CPE to at best 3. */
+
+/* C.The CPE is 1.00 for integer data. */
+
+/* D. The multiplications performed by this routine are of the general form udata[i]*vdata[i]. 
+   These are logically independent of each other. Hence the multiplier can execute them in a pipelined fashion. */
+
+/* 5.16 */
+void inner4(vec_ptr u, vec_ptr v, data_t *dest)
+{
+	 long int i;
+	 int length = vec_length(u);
+	 data_t *udata = get_vec_start(u);
+	 data_t *vdata = get_vec_start(v);
+	 data_t sum = (data_t) 0;
+
+	 for (i = 0; i < length - 3; i += 4) {
+		  sum  += udata[i] * vdata[i]
+			   + udata[i+1] * vdata[i+1]
+			   + udata[i+2] * vdata[i+2]
+			   + udata[i+3] * vdata[i+3];
+	 }
+
+	 for(; i < length; i++)
+		  sum += udata[i] * vdata[i];
+	 
+	 *dest = sum;
+}
+
+/* A. We must perform two loads per element to read values for udata and vdata. There is only one unit to perform these loads, and it requires one cycle. */
+/* B. float computation can't associate.Therefore a latency of float addition is still CPE of 3.00 */
+
+
