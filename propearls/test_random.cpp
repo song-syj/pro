874e57d0de159345266207cde24402a1e64e321c
song-syj <yangjiesun2012@gmail.com>
2014-03-14 21:18:42 +0800

    programing pearls

---
+++ propearls/test_random.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<set>
+#include<cstdio>
+#include<cstdlib>
+#include<ctime>
+#include"random.h"
+
+using namespace std;
+
+
+int main()
+{
+	genfloyd(2,3);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
