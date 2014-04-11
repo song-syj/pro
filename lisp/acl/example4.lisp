03be6264c83a0e23ad4c49b94021f29eee5f8d83
song-syj <yangjiesun2012@gmail.com>
2014-04-11 10:58:34 +0800

    add example for binary search tree

--- lisp/acl/example4.lisp
+++ lisp/acl/example4.lisp
@@ -1,6 +1,6 @@
 ;;4.5 Example: Parsing Dates
 
-(defun tokens (str test start)
+#|(defun tokens (str test start)
   (let ((p1 (position-if test str :start start)))
 	(if p1
 		(let ((p2 (position-if #'(lambda (c)
@@ -31,29 +31,29 @@
 					 :test #'string-equal)))
 	(if p
 		(+ p 1)
-		nil)))
+		nil))) |#
 
 ;;4.7 Example: Binary Search Trees
 
 (defstruct (node (:print-function
 				  (lambda (n s d)
-					(format s "#<~A>" (node-data n)))))
-  (data nil) (l nil) (r nil))
+					(format s "#<~A>" (node-elt n)))))
+  (elt nil) (l nil) (r nil))
 
 (defun bst-insert (obj bst <)
   (print bst)
   (if (null bst)
-	  (make-node :data obj)
-	  (let ((elt (node-data bst)))
+	  (make-node :elt obj)
+	  (let ((elt (node-elt bst)))
 		(if (eql obj elt)
 			bst
 			(if (funcall < obj elt)
 				(make-node
-				 :data elt
+				 :elt elt
 				 :l (bst-insert obj (node-l bst) <)
 				 :r (node-r bst))
 				(make-node
-				 :data elt
+				 :elt elt
 				 :r (bst-insert obj (node-r bst) <)
 				 :l (node-l bst)))))))
 
@@ -61,9 +61,71 @@
 (defun bst-find (obj bst <)
   (if (null bst)
 	  nil
-	  (let ((elt (node-data bst)))
+	  (let ((elt (node-elt bst)))
 		(if (eql obj elt)
 			bst
 			(if (funcall < obj elt)
 				(bst-find obj (node-l bst) <)
 				(bst-find obj (node-r bst) <))))))
+
+(defun bst-min (bst)
+  (and bst
+	   (or (bst-min (node-l bst)) bst)))
+
+(defun bst-max (bst)
+  (and bst
+	   (or (bst-max (node-r bst)) bst)))
+
+(defun bst-remove (obj bst <)
+  (if (null bst)
+	  nil
+	  (let ((elt (node-elt bst)))
+		(if (eql obj elt)
+			(percolate bst)
+			(if (funcall < obj elt)
+				(make-node
+				 :elt elt
+				 :l (bst-remove obj (node-l bst) <)
+				 :r (node-r bst))
+				(make-node
+				 :elt elt
+				 :r (bst-remove obj (node-r bst) <)
+				 :l (node-l bst)))))))
+
+(defun percolate (bst)
+  (let ((l (node-l bst))
+		(r (node-r bst)))
+	(cond ((null l) r)
+		  ((null r) l)
+		  (t (if (zerop (random 2))
+				 (make-node
+				  :elt (node-elt (bst-max l))
+				  :r r
+				  :l (bst-remove-max l))
+				 (make-node
+				  :elt (node-elt (bst-min r))
+				  :l l
+				  :r (bst-remove-min r)))))))
+
+(defun bst-remove-max (bst)
+  (if (null (node-r bst))
+	  (node-l bst)
+	  (make-node
+	   :elt (node-elt bst)
+	   :l (node-l bst)
+	   :r (bst-remove-max (node-r bst)))))
+
+(defun bst-remove-min (bst)
+  (if (null (node-l bst))
+	  (node-r bst)
+	  (make-node
+	   :elt (node-elt bst)
+	   :l (bst-remove-min (node-l bst))
+	   :r (node-r bst))))
+
+(defun bst-traverse (fn bst)
+  (when bst
+	(bst-traverse fn (node-l bst))
+	(funcall fn (node-elt bst))
+	(bst-traverse fn (node-r bst))))
+
