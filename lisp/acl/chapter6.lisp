29cf5b9e258a52e7dc1919c15a89299d83a159b6
song-syj <yangjiesun2012@gmail.com>
2014-04-24 11:34:39 +0800

    complete exercise 6

--- lisp/acl/chapter6.lisp
+++ lisp/acl/chapter6.lisp
@@ -33,3 +33,61 @@
 					(bin-search obj vec :key key :test test :start (+ mid 1) :end end)
 					(aref vec mid))))))))
 
+;; 3
+(defun n-arv (&rest arg)
+  arg)
+
+;; 4 most
+
+(defun most (fn lst)
+  (if (null lst)
+	  (values nil nil)
+	  (let* ((n1 (car lst))
+			 (n2 (cadr lst))
+			 (fir (funcall fn n1))
+			 (sec (funcall fn n2)))
+		   (when (< fir sec)
+			 (rotatef fir sec)
+			 (rotatef n1 n2))
+		   (dolist (x lst (values n1 n2))
+			 (let ((val (funcall fn x)))
+			   (cond ((< fir val) (setf n2 n1 sec fir n1 x fir val))
+					 ((< sec val) (setf n2 x sec x))
+					 (t nil)))))))
+;;5
+(defun my-remove-if (fn lst)
+  (let ((acc nil))
+	(dolist (x lst)
+	  (and (null (funcall fn x))
+		   (push x acc)))
+	(nreverse acc)))
+
+;; 6
+(let ((max nil))
+  (defun max-so-for(num)
+	(if (null max)
+		(setf max num)
+		(if (< max num)
+			(setf max num)))
+	max))
+
+;; 7
+(let ((prev nil))
+  (defun larger-prev (num)
+	(let ((prev1 prev))
+	  (setf prev num)
+	  (and prev1
+		   (< prev1 num)))))
+
+;; 8
+
+(let ((vec (make-array 101 :intial-element nil)))
+  (defun frugal (n)
+	(when (null (aref vec n))
+	  (setf (aref vec n) t)
+	  (expensive n))))
+
+;; 9
+(defun my-apply (fn &rest arg)
+  (let ((*print-base* 8))
+	(print (apply fn arg))))
